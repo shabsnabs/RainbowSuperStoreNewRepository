487195df648b8d7876d53fd6011ce6be46ece146
shabsnabs <shabanassl@gmail.com>
2023-07-25 13:43:29 +0530

    commiiting normal java class

---
+++ com.gittest/src/main/java/com/gittest/Newsrc.java
@@ -0,0 +1,11 @@
+package com.gittest;
+
+public class Newsrc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("1234");
+	System.out.println("hi  1234");
+
+	}
+}
